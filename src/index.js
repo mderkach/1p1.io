dd247da9a134142684a6abf06932f69ec5af0338
Деркач Михаил Игоревич <mderkach@ncfu.net>
2019-03-07 16:32:33 +0300

    slider reviews

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import './js/common'
 import './scss/main.scss'
 // js component
 import './templates/components/slider/slider-bg'
+import './templates/components/slider/slider-reviews-landing'
 //pages
 var template = require('./index.twig')
---
+++ src/templates/components/slider/slider-reviews-landing.js
@@ -0,0 +1,22 @@
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.css'
+
+var sliderRviewLanding = new Swiper('.slider-reviews-landing__container', {
+  slidesPerView: 1,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 10000,
+    speed: 600,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: '.review__dots',
+    clickable: true,
+    bulletClass: 'review-bullet',
+    bulletActiveClass: 'review-bullet-active',
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>';
+    },
+  },
+});
