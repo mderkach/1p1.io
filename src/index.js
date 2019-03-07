cc1cb3595aa5dfff3a6908a1c6d61da7b64a4417
Skeev_ <derkach94@gmail.com>
2019-03-08 01:26:44 +0300

    slider partners finish

--- src/index.js
+++ src/index.js
@@ -5,5 +5,6 @@ import './scss/main.scss'
 // js component
 import './templates/components/slider/slider-bg'
 import './templates/components/slider/slider-reviews-landing'
+import './templates/components/slider/slider-partners'
 //pages
 var template = require('./index.twig')
--- src/templates/components/slider/slider-partners.js
+++ src/templates/components/slider/slider-partners.js
@@ -1,2 +1,12 @@
 import Swiper from 'swiper';
 import 'swiper/dist/css/swiper.css'
+
+var sliderPartners = new Swiper('.slider__partners-container', {
+  slidesPerView: 8,
+  loop: true,
+  autoplay: {
+    delay: 1800,
+    speed: 800,
+    disableOnInteraction: false,
+  },
+});
