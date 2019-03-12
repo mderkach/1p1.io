3bc15ff854a018110735b93e98d98e73b77d6a86
Деркач Михаил Игоревич <mderkach@ncfu.net>
2019-03-12 18:17:45 +0300

    video-player finish

--- src/js/common.js
+++ src/js/common.js
@@ -119,5 +119,38 @@ var video = document.querySelector('#video');
 // }); 
 
 $('.btn-video').fancybox({
+  modal: false,
+  hideScrollbar: true,
   buttons: ["close"],
+  video: {
+    autoStart: false
+  },
+  onActivate: function() {
+    playButton.classList.remove('is-played');
+  }
+
 });
+var playButton = document.querySelector("#play-pause");
+//videoplayer
+window.onload = function () {
+
+  // Video
+  var video = document.querySelector("video");
+  video.controls = false;
+  video.autoplay = false;
+  // Buttons
+  
+  // Event listener for the play/pause button
+  playButton.addEventListener("click", function () {
+    if (video.paused == true) {
+      // Play the video
+      video.play();
+      playButton.classList.toggle('is-played');
+    } else {
+      // Pause the video
+      video.pause();
+    }
+  });
+}
+
+
