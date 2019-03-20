2195cae7772c3f5f9d1de7f3463eb536262a79f9
Skeev_ <derkach94@gmail.com>
2019-03-20 21:25:57 +0300

    bookmark finish

--- src/js/bookmark.js
+++ src/js/bookmark.js
@@ -1,14 +1,19 @@
 var bookmark = document.querySelectorAll('.btn-bookmark');
 var userStatus = document.querySelector(".header");
-var error = document.querySelector('.ads__bookmark-error');
+var error = document.querySelector('.bookmark-error');
 bookmark.forEach(
   function(btn) {
     btn.addEventListener("click", function () {
       //AJAX sucees here
-  
+      if (userStatus.classList.contains("--authorised")){
+        this.querySelector('.ads__bookmark-img').classList.toggle('is-bookmarked');
+      }
       //AJAX false here
       if (userStatus.classList.contains("--unauthorised")){
         error.classList.toggle('is-error');
+        setTimeout(function() {
+          error.classList.toggle('is-error');
+        }, 3000);
       }
     });
   }
