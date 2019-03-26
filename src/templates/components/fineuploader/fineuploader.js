28577bb390299c3a44408eeda1021e6e07dbc764
Skeev_ <derkach94@gmail.com>
2019-03-27 02:58:02 +0300

    fineuploader w/o blur onload and error

--- src/templates/components/fineuploader/fineuploader.js
+++ src/templates/components/fineuploader/fineuploader.js
@@ -1,10 +1,13 @@
 import qq from 'fine-uploader/lib/traditional'
 
-var uploader = new qq.FineUploader({
-  element: document.querySelector('#uploader'),
-  template: document.querySelector('.qq-template-gallery'),
-  multiple: false,
-  enableTooltip: false,
-});
-
-console.log(uploader);
+var photos = document.querySelectorAll('.fineuploader');
+photos.forEach(
+  function(uploader) {
+    var photo = new qq.FineUploader({
+      element: uploader,
+      template: document.querySelector('.qq-template-gallery'),
+      multiple: false,
+      enableTooltip: false,
+    });
+  }
+);
