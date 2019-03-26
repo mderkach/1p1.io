c304d9b294a32f95c7f5a1cf211da2bfd55a5664
Skeev_ <derkach94@gmail.com>
2019-03-26 22:13:34 +0300

    macro donate

--- src/js/input-number-controls.js
+++ src/js/input-number-controls.js
@@ -1,35 +1,22 @@
 var controls = document.querySelectorAll('.form__popup-inputNumberMini-controls');
 var input = document.querySelectorAll('.form__popup-inputNumberMini');
 controls.forEach(
-  function(control) {
+  function (control) {
     var inputValue = control.parentNode.parentNode.querySelector('input');
     var action = control.getAttribute('data-action');
-    control.addEventListener('click', function(){
+    control.addEventListener('click', function () {
       if (action == 'increase') {
-        var integer = parseInt(inputValue.value);
-        inputValue.value = integer += 1;
-        inputValue.setAttribute('value', parseInt(inputValue.value));
-        //inputValue.stepUp();
+        // var integer = parseInt(inputValue.value);
+        // inputValue.value = integer += 1;
+        // inputValue.setAttribute('value', parseInt(inputValue.value));
+        inputValue.stepUp();
+        inputValue.onchange = console.log('plus');
       } else {
-        var integer = parseInt(inputValue.value);
-        inputValue.value = integer -= 1;
-        inputValue.setAttribute('value', parseInt(inputValue.value));
-        //inputValue.stepDown();
+        // var integer = parseInt(inputValue.value);
+        // inputValue.value = integer -= 1;
+        // inputValue.setAttribute('value', parseInt(inputValue.value));
+        inputValue.stepDown();
       }
     });
-  }
-);
-// TO:DO сделать реплейс процентов
-input.forEach (
-  function (input) {
-    var type = input.getAttribute('data-type');
-    if (type == 'percent') {
-      console.log(type);
-      this.addEventListener('change', function(){
-        var currentValue = parseInt(this.value);
-        this.value = currentValue + '%';
-      });
-    }
-    
   }
 );
