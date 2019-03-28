f2bb8888b61a2a5978c3945ac76b73734b61546d
Skeev_ <derkach94@gmail.com>
2019-03-29 01:12:50 +0300

    sell item desktop css fix, js fix

--- src/js/input-number-controls.js
+++ src/js/input-number-controls.js
@@ -1,21 +1,46 @@
 var controls = document.querySelectorAll('.form__popup-inputNumberMini-controls');
 var input = document.querySelectorAll('.form__popup-inputNumberMini');
+
+var percent = document.querySelector('[data-type=percent]');
+
+function setValue() {
+  var currentValue = percent.value;
+  if (percent.value > 100) {
+    percent.value = 100;
+  } else {
+    percent.value = parseInt(currentValue);
+  }
+  percent.setAttribute('value', parseInt(percent.value));
+}
+percent.addEventListener('input', function () {
+  setValue();
+});
+
+percent.addEventListener('blur', function () {
+  if (percent.value == "") {
+    percent.value = 0;
+    percent.setAttribute('value', parseInt(percent.value));
+  } else {
+    setValue();
+  }
+});
+
 controls.forEach(
   function (control) {
     var inputValue = control.parentNode.parentNode.querySelector('input');
     var action = control.getAttribute('data-action');
+
     control.addEventListener('click', function () {
       if (action == 'increase') {
         // var integer = parseInt(inputValue.value);
         // inputValue.value = integer += 1;
-        // inputValue.setAttribute('value', parseInt(inputValue.value));
         inputValue.stepUp();
-        inputValue.onchange = console.log('plus');
+        inputValue.setAttribute('value', parseInt(inputValue.value));
       } else {
         // var integer = parseInt(inputValue.value);
         // inputValue.value = integer -= 1;
-        // inputValue.setAttribute('value', parseInt(inputValue.value));
         inputValue.stepDown();
+        inputValue.setAttribute('value', parseInt(inputValue.value));
       }
     });
   }
