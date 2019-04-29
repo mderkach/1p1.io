be19c8504457c67fd4e0fa0a518591a5ea9903e6
mderkach <derkach94@gmail.com>
2019-04-29 17:22:25 +0300

    cards bookmark

---
+++ src/templates/components/slider/slider-card-bookmarks.js
@@ -0,0 +1,19 @@
+import Swiper from 'swiper';
+import 'swiper/dist/css/swiper.css';
+
+var sliderBookmarks = document.querySelectorAll('.card-bookmarks__slider-container');
+
+if (sliderBookmarks) {
+  sliderBookmarks.forEach(function(slider) {
+    var cardBookmarksSLider = new Swiper(slider, {
+      slidesPerView: 1,
+      wrapperClass: 'card-bookmarks__slider-wrapper',
+      slideClass: 'card-bookmarks-img',
+      navigation: {
+        nextEl: '.card-bookmarks__slider-button-next',
+        prevEl: '.card-bookmarks__slider-button-prev',
+        disabledClass: '--disabled'
+      },
+    });
+  });
+}
