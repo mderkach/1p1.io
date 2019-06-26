cfc2fe30dce9fec3c4f2d545f7ecd1ca859514c1
Skeev_ <derkach94@gmail.com>
2019-06-26 18:43:37 +0300

    header fix, pagination fix

--- src/js/nabar-hover-catalogue-dropdown.js
+++ src/js/nabar-hover-catalogue-dropdown.js
@@ -1,37 +1,32 @@
-var catalogueTrigger = document.querySelector('.dropdown-catalogue-trigger');
-
-
+const catalogueTrigger = document.querySelector('.dropdown-catalogue-trigger')
 if (catalogueTrigger) {
-  var catalogue = document.querySelector('.dropdown-catalogue');
-  var container = catalogue.querySelector('.dropdown-catalogue__container')
-  var menuItem = document.querySelectorAll('.nav__menu-item');
-  
-  catalogueTrigger.addEventListener('click', function (event) {
-    event.preventDefault();
-  });
+  const catalogue = document.querySelector('.dropdown-catalogue')
+  const container = catalogue.querySelector('.dropdown-catalogue__container')
+  const menuItem = document.querySelectorAll('.nav__menu-item')
+  const isActiveMenuItem = document.querySelector('.nav__menu-item.is-active')
+  const logo = document.querySelector('.logo__wrapper')
 
-  catalogueTrigger.addEventListener('mouseover', function (event) {
-    catalogueTrigger.classList.add('is-active');
-    catalogue.classList.add('is-show');
-  });
-
-  container.addEventListener('mouseleave', function (event) {
-    catalogueTrigger.classList.remove('is-active');
-    catalogue.classList.remove('is-show');
-  });
-
-  for (var i = 0; i < menuItem.length; i++) {
-    if (menuItem[i] == catalogueTrigger) {
-
-    } else {
-      menuItem[i].addEventListener('mouseover', function () {
-        catalogueTrigger.classList.remove('is-active');
-        catalogue.classList.remove('is-show');
-      });
-      document.querySelector('.logo__wrapper').addEventListener('mouseover', function(){
-        catalogueTrigger.classList.remove('is-active');
-        catalogue.classList.remove('is-show');
-      });
+  logo.addEventListener('mouseover', () => {
+    if (isActiveMenuItem !== catalogueTrigger) {
+      catalogueTrigger.classList.remove('is-active')
     }
-  }
+    catalogue.classList.remove('is-show')
+  })
+  catalogueTrigger.addEventListener('click', e => e.preventDefault())
+  catalogueTrigger.addEventListener('mouseover', () => catalogue.classList.add('is-show'))
+  // catalogueTrigger.addEventListener('mouseleave', () => catalogue.classList.remove('is-show'))
+  container.addEventListener('mouseleave', () => {
+    catalogue.classList.remove('is-show')
+  })
+  container.addEventListener('mouseover', () => catalogueTrigger.classList.add('is-active'))
+  menuItem.forEach(el => {
+    if (el !== catalogueTrigger) {
+      el.addEventListener('mouseover', () => {
+        if (isActiveMenuItem !== catalogueTrigger) {
+          catalogueTrigger.classList.remove('is-active')
+        }
+        catalogue.classList.remove('is-show')
+      })
+    }
+  })
 }
